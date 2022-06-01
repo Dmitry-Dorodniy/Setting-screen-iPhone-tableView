47c7208440c11f6234ab67c4e7d19896a3e321d2
DmitryDorodniy <dmitrydorodniy@gmail.com>
2022-06-01 17:27:44 +0300

    Add badge. Refactoring code

--- IOS6=HW13-DmitryDorodniy/AppDelegate.swift
+++ IOS6=HW13-DmitryDorodniy/AppDelegate.swift
@@ -12,14 +12,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         setWindow()
         return true
     }
 
     func setWindow() {
-
         let navigationController = UINavigationController(rootViewController: TableViewController())
 
         window = UIWindow(frame: UIScreen.main.bounds)
--- IOS6=HW13-DmitryDorodniy/TableViewController.swift
+++ IOS6=HW13-DmitryDorodniy/TableViewController.swift
@@ -11,11 +11,10 @@ class TableViewController: UIViewController {
 
     private let allCellData = CellApi.getData()
 
-    //    создаём свойство класса с таблицей
     private lazy var tableView: UITableView = {
-        //        таблица с закруглёнными краями групп
-        let table = UITableView(frame: view.bounds, style: .insetGrouped)
-
+        // таблица с закруглёнными краями групп
+        let table = UITableView(frame: view.bounds,
+                                style: .insetGrouped)
         table.dataSource = self
         table.delegate = self
         return table
@@ -30,18 +29,16 @@ class TableViewController: UIViewController {
         setupLayout()
     }
 
-    // MARK: - Settings
+    // MARK: - Settings of table
     private func setupView() {
 
         view.backgroundColor = .systemBackground
-        navigationItem.title = "Настройки"
-        //        регистрируем таблицу
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: Metric.reuseString)
-        tableView.register(CustomTableViewCell.self, forCellReuseIdentifier: CustomTableViewCell.identifier)
+        navigationItem.title = Metric.navigationTitle
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: Metric.defaultCellReuseIdentifier)
+        tableView.register(CustomTableViewCell.self, forCellReuseIdentifier: Metric.customCellReuseIdentifier)
     }
 
     private func setupHierarchy() {
-
         view.addSubview(tableView)
     }
 
@@ -59,6 +56,7 @@ class TableViewController: UIViewController {
 }
 
 // MARK: - Table extentions
+
 extension TableViewController: UITableViewDataSource  {
 
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -71,59 +69,86 @@ extension TableViewController: UITableViewDataSource  {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cellDataIndex = allCellData[indexPath.section][indexPath.row]
 
+        // MARK: - Set custom cell table
+
         if cellDataIndex.isCustomCell != nil {
-            let cell = tableView.dequeueReusableCell(withIdentifier: CustomTableViewCell.identifier, for: indexPath) as! CustomTableViewCell
+            let cell = tableView.dequeueReusableCell(withIdentifier: Metric.customCellReuseIdentifier,
+                                                     for: indexPath) as! CustomTableViewCell
+
             cell.accessoryType = .disclosureIndicator
             cell.cellData = cellDataIndex
+
             if cellDataIndex.isToggle != nil {
                 let switchButton = UISwitch(frame: .zero)
                 switchButton.addTarget(self, action: #selector(didChangeSwitch(_:)), for: .valueChanged)
                 cell.accessoryView = switchButton
             }
-
             return cell
         }
 
-        //стиль ячейки таблицы
-        let cell = UITableViewCell(style: .value1, reuseIdentifier: Metric.reuseString)
-        // let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        // MARK: - Set default cell table
 
+        let cell = UITableViewCell(style: .value1,
+                                   reuseIdentifier: Metric.defaultCellReuseIdentifier)
         //стиль контента в ячейках
         cell.accessoryType = .disclosureIndicator
-        //текст тайтла и описания
+
+        // MARK: - Set icon color frame
 
         let colorView = UIView()
         colorView.layer.masksToBounds = true
-        colorView.layer.cornerRadius = 4
-        colorView.backgroundColor = cellDataIndex.iconColor ?? .tintColor
+        colorView.layer.cornerRadius = Metric.iconColorViewCornerRadius
+        colorView.backgroundColor = cellDataIndex.iconColor
         cell.addSubview(colorView)
 
+        // MARK: - Set default cell content
         var content = cell.defaultContentConfiguration()
         let image = UIImage(systemName: cellDataIndex.image ?? "")
-        //        let image = UIImage(systemName: cellDataIndex.image ?? "")
 
-        //        content.imageProperties.cornerRadius = 40
         content.imageProperties.tintColor = .white
-        content.imageProperties.maximumSize = CGSize(width: 19, height: 19)
+        content.imageProperties.maximumSize = CGSize(width: Metric.defaultCelliConSymbolSize,
+                                                     height: Metric.defaultCelliConSymbolSize)
         content.image = image
         content.text = cellDataIndex.title
         content.secondaryText = cellDataIndex.detail ?? ""
 
         cell.contentConfiguration = content
 
-        //switch button
-        //        let switchButton = UISwitch(frame: .zero)
+        // MARK: - Set switch button
+
         if cellDataIndex.isToggle != nil {
             let switchButton = UISwitch(frame: .zero)
             switchButton.addTarget(self, action: #selector(didChangeSwitch(_:)), for: .valueChanged)
             cell.accessoryView = switchButton
         }
 
+        // MARK: - Set Badge
+
+        if let badge = cellDataIndex.badge {
+            let badgeButton = UIButton(type: .custom)
+            UIView.performWithoutAnimation {
+                badgeButton.setTitle(title, for: .normal)
+                badgeButton.layoutIfNeeded()
+            }
+            var config = UIButton.Configuration.filled()
+            config.title = String(badge)
+            config.baseBackgroundColor = .systemRed
+            config.buttonSize = .mini
+            badgeButton.configuration = config
+
+            cell.addSubview(badgeButton)
+            badgeButton.translatesAutoresizingMaskIntoConstraints = false
+            badgeButton.rightAnchor.constraint(equalTo: cell.contentView.rightAnchor, constant: -8).isActive = true
+            badgeButton.centerYAnchor.constraint(equalTo: cell.contentView.centerYAnchor).isActive = true
+        }
+
+        // MARK: - Setup layout
+
         colorView.translatesAutoresizingMaskIntoConstraints = false
         colorView.centerYAnchor.constraint(equalTo: cell.contentView.centerYAnchor).isActive = true
-        colorView.leftAnchor.constraint(equalTo: cell.contentView.layoutMarginsGuide.leftAnchor, constant: -1).isActive = true
-        colorView.heightAnchor.constraint(equalToConstant: 26).isActive = true
-        colorView.widthAnchor.constraint(equalToConstant: 26).isActive = true
+        colorView.leftAnchor.constraint(equalTo: cell.contentView.layoutMarginsGuide.leftAnchor, constant: -3).isActive = true
+        colorView.heightAnchor.constraint(equalToConstant: Metric.iconColorViewSize).isActive = true
+        colorView.widthAnchor.constraint(equalToConstant: Metric.iconColorViewSize).isActive = true
 
         return cell
     }
@@ -133,7 +158,7 @@ extension TableViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         let title = allCellData[indexPath.section][indexPath.row].title
-        print("\(title) cell pressed")
+        print("Выбрана ячейка \(title)")
     }
 }
 
